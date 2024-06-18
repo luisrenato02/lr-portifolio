3cc03d9e5ea2b63c5c83a5e8a8a769744a266d30
Luís Renato <89888509+luisrenato02@users.noreply.github.com>
2024-06-18 18:26:14 -0300

    chore: metadata

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -4,6 +4,7 @@ import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 import "./globals.css";
 import { Metadata } from "next";
+import image from "../../../public/image.svg";
 
 export const metadata: Metadata = {
   title: {
@@ -18,7 +19,7 @@ export const metadata: Metadata = {
     siteName: "Luís Renato - Portfólio",
     images: [
       {
-        url: "https://og-playground.vercel.app/?share=xVRLjtswDL2KoFULRLWbZIrUSGdTzK5FgSLLbGSLjjXRx7DoOsFgDtMD9BRzsVJRPs4gGHTXhS3Sen4kHyk98cor4AXPMvbdK11rUKyUgd7esQaxDUWWodRm0E71-kPlbUZP6x04DJmV3RZQu00WoELtXcgqlOLkrN3aLZX-xXD4sua1gR2LL1F5wwZR98awJi0awQZRESl07LEPqOv9yS03YmgIsOb3a8fYhZA2Np3ci7v8QHukPMLGwOvIVhUHu_PDKYt2Lz5OWbsT87h7M5kScABwrBWLcwSK0UxvhUDYoZjtDAu2ONhzsmvvUJTeKIadrLakmkC9aTChg5EI4nOeJ9dAjaM4FCm00t3_MC-_J8wCqxpp_TI7fHwFSgkl0u1ezPKciL71L38C-wlOomcr77RE_er3ZdZML85ZPItikfQlaciJbKOI1yJ3vncKlLCKhUYqP1yBCS5H4LeaPiIqfafiGBwWQdo5SrsjWJyMJNsnko26dxcbOUsCxilOkltQurfp62mQvnrXQAXdMpPjYjKq5mZx1hDvf6ow9fDf6jtPEWX14IipY2BZRTDq-1vFjr2LfbLSyifct4eDzYsnPmiFDS8WeT7hDcRB5sU8OgrKfsOLWpoAEw7WP-rVvo23DJ2g6BFPTPzBlqB4gV0PzxOOsiTE6njVsHewa6HTlkSQ5j1__gs",
+        url: image,
         width: 800,
         height: 400,
       },
